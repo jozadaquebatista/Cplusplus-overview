b4513290cbd44462d0e8dfb2eeab9216ccf1dc9c
jozadaquebatista <jozadaquebatista@gmail.com>
2015-07-31 16:44:23 -0300

    Operators

---
+++ src/Operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/* To compile it use: g++ -o name_your_file name_file.cpp -std=c++11 */
+
+/* +, -, *, // */
+
+int main()
+{
+
+  int myAge = 39;
+  
+  float favNum = 3.141592;
+  
+  double otherFavNum = 1.6180339887;
+  
+  int five = 5;
+
+  std::cout << "5++=" << five++ << std::endl;
+  std::cout << "++5=" << ++five << std::endl;
+  std::cout << "5--=" << five-- << std::endl;
+  std::cout << "--5=" << --five << std::endl;
+  
+  std::cout << "The result of(1+2-3*4/5): " << 1+2-3*4/5 << std::endl;
+  
+  return(0);
+
+}

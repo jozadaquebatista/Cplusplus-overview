c9c19582c4c873b17c1e4e27719de510d23e1327
jozadaquebatista <jozadaquebatista@gmail.com>
2015-07-31 16:34:22 -0300

    dataTypes

---
+++ dataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+  int myAge = 39;
+  
+  float favNum = 3.141592;
+  
+  double otherFavNum = 1.6180339887;
+
+  std::cout << "Your Favorite Number is: " << favNum << std::endl;
+  
+  return(0);
+
+}

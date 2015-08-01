57a3aee32af40cba86eaff6f4feec0342442f7ba
jozadaquebatista <jozadaquebatista@gmail.com>
2015-08-01 18:35:44 -0300

    doWhileLoops

---
+++ src/doWhileLoops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+/* To compile it use: g++ -o name_your_file name_file.cpp -std=c++11 */
+
+int main()
+{
+
+  std::string numberGuessed;
+  int intNumberGuessed = 0;
+  
+  do {
+  
+    std::cout << "Guess between 1 and 10: ";
+    
+    getLine(cin, numberGuessed);
+    
+    intNumberGuessed = stoi(numberGuessed);
+  
+  } while(intNumberGuessed != 4)
+  
+  std::cout << "You win." << std::endl;
+  
+  return(0);
+
+}
